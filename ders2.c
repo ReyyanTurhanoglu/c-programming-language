98d0bc23e4c95b815fb835e6e510ee65d29695c6
Reyyan Turhanoğlu <143796621+ReyyanTurhanoglu@users.noreply.github.com>
2023-09-01 12:56:09 +0300

    Add files via upload

---
+++ ders2.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#define PI 3.14
+int main() {
+	
+float sayi1;
+float sayi2;
+float sayi3;
+float ortalama;
+printf("sayi1i giriniz:\n");
+scanf("%f",&sayi1);
+printf("sayi2 yi giriniz:\n");
+scanf("%f",&sayi2);
+printf("sayi3 u giriniz:\n");
+scanf("%f",&sayi3);
+ortalama=sayi1/3+sayi2/3+sayi3/3;
+printf("ortalama %f dir.",ortalama);
+
+
+
+	
+	
+	
+	
+	
+	
+	return 0;
+}
